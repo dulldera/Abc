c35cd55a57fe23f25797c3e2de01400a976ae73a
dulldera <dulldera@ik1-337-28654.vs.sakura.ne.jp>
2018-08-05 02:29:10 +0900

    cgi

---
+++ www/html/ruby.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+print "Content-type: text/html\n\n"
+print "<html>\n"
+print '<head><meta charset="UTF-8"></head>' + "\n"
+print "<body>"
+print "Hello, World! World!\n"
+print "</body>\n"
+print "</html>\n"
